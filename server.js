cefc9407da0451801d4dcd41bd35adb8057d366d
wasim-redalkemi <wasim.redalkemi@gmail.com>
2022-06-23 14:58:10 +0530

    server changes

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const compression = require('compression');
+
+app.use(compression());
+
+//static path
+app.use(express.static(path.join(__dirname, 'build')));
+
+//server the index page
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+//set port for server
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
