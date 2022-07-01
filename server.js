0d3218e11b3bc79b4e6ca474e0a2f78081bee0b9
wasim-redalkemi <wasim.redalkemi@gmail.com>
2022-07-01 13:11:12 +0530

    changes

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const compression = require('compression');
 
 app.use(compression());
 
+app.use((req, res, next) => {
+  if(req.secure !== true ){
+    res.redirect(301, `https://${req.headers.host}${req.url}`);
+    console.log(`https://${req.headers.host}${req.url}`);
+  } 
+  else {
+    next();
+  }
+});
+
 //static path
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -13,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
 
 //set port for server
 app.listen(PORT, () => {
