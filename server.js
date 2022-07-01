3e47a98673a7bfc32c476d6dab6f0a09388a5ab1
wasim-redalkemi <wasim.redalkemi@gmail.com>
2022-07-01 13:39:56 +0530

    changes

--- server.js
+++ server.js
@@ -2,17 +2,23 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const env = process.env.NODE_ENV || "production";
 const compression = require('compression');
 
 app.use(compression());
 
 app.use((req, res, next) => {
-  if(req.secure !== true ){
-    res.redirect(301, `https://${req.headers.host}${req.url}`);
-    console.log(`https://${req.headers.host}${req.url}`);
-  } 
-  else {
-    next();
+  if(env === "production"){
+    if (req.headers.host.slice(0, 4) === "www.") {
+      var newHost = req.headers.host.slice(4);
+      res.redirect(301, req.protocol + "://" + newHost + req.originalUrl);
+    }
+    else if(req.secure !== true ){
+      res.redirect(301, `https://${req.headers.host}${req.url}`);    
+    } 
+    else {
+      next();
+    }
   }
 });
 
